21bb14158af4b00fcea79fa4d0a1e80cc38e658c
Pamela Bute <pamelabute02@gmail.com>
2023-03-28 15:36:25 +0200

    created a login page
    users can create an account and log in

---
+++ script.js
@@ -0,0 +1,30 @@
+//const createacctbtn =document.getElementById("create-acct-btn");
+// const returnBtn = document.getElementById("return-btn");
+// //const submitButton = document.getElementById("submit");
+// const signupButton = document.getElementById("sign-up");
+// const main = document.getElementById("main");
+// const createacct = document.getElementById("create-acct");
+
+
+function showSignUp() {
+    const main = document.getElementById("main");
+    const createacct = document.getElementById("create-acct");
+    main.style.display = "none";
+    createacct.style.display= "block";
+    
+}
+
+function showLogin() {
+    const main = document.getElementById("main");
+    const createacct = document.getElementById("create-acct");
+    main.style.display = "block";
+    createacct.style.display= "none";
+}
+
+// signupButton.addEventListener("click",()=>{
+    
+
+// });
+// returnBtn.addEventListener("click",function(){
+  
+// });
